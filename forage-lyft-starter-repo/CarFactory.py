d02b5aac6e0077a0d5bec83ba6878e52f78819e6
keishev <keisha.als118@gmail.com>
2023-12-03 20:08:44 +0800

    final

--- forage-lyft-starter-repo/CarFactory.py
+++ forage-lyft-starter-repo/CarFactory.py
@@ -7,29 +7,35 @@ from battery.nubbin_battery import NubbinBattery
 
 from car import Car
 
-def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
-    engine = CapuletEngine(last_service_date, current_mileage, last_service_mileage)
-    battery = SpindlerBattery(last_service_date, current_date)
-    Car (engine, battery)
+class CarFactory:
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(last_service_date, current_mileage, last_service_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        Car (engine, battery)
 
-def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
-    engine = WilloughbyEngine(last_service_mileage, current_mileage)
-    battery = SpindlerBattery(last_service_date, current_date)
-    Car (engine, battery)
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+        Car (engine, battery)
 
-def create_palindrome(current_date, last_service_date, warning_light_on):
-    engine = SternmanEngine(warning_light_on)
-    battery = SpindlerBattery(last_service_date, current_date)
-    Car (engine, battery)
+    @staticmethod
+    def create_palindrome(current_date, last_service_date, warning_light_on):
+        engine = SternmanEngine(warning_light_on)
+        battery = SpindlerBattery(last_service_date, current_date)
+        Car (engine, battery)
 
-def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
-    engine = WilloughbyEngine(last_service_mileage, current_mileage)
-    battery = NubbinBattery(last_service_date, current_date)
-    Car (engine, battery)
+    @staticmethod
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        Car (engine, battery)
 
-def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
-    engine = CapuletEngine(last_service_mileage, current_mileage)
-    battery = NubbinBattery(last_service_date, current_date)
-    Car (engine, battery)
+    @staticmethod
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(last_service_mileage, current_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+        Car (engine, battery)
 
 
--- forage-lyft-starter-repo/battery.py
+++
@@ -1,3 +0,0 @@
-class battery():
-    def __init__(self, battery_type):
-        self.battery_type = battery_type
